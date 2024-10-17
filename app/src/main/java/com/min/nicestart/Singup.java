e2548873b26420d703e79685a532c2b793c53b2f
Min Wang <minwang807@gmail.com>
2024-10-17 12:18:15 +0200

    Añadido ventana de sign up, dentro de el se ha añadido un confirm password y un mail. Cambio en botones y en login, se puede ir a sign up dandole al boton

--- app/src/main/java/com/min/nicestart/Singup.java
+++ app/src/main/java/com/min/nicestart/Singup.java
@@ -1,8 +1,6 @@
 package com.min.nicestart;
 
-import android.content.Intent;
 import android.os.Bundle;
-import android.view.View;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -10,24 +8,17 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
-import com.min.nicestart.R;
-
-public class Login extends AppCompatActivity {
+public class Singup extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
-        setContentView(R.layout.login);
+        setContentView(R.layout.activity_singup);
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
-    }
-    public void AbrirSignup (View v)
-    {
-        Intent intent = new Intent(Login.this,Singup.class);
-        startActivity(intent);
     }
 }
