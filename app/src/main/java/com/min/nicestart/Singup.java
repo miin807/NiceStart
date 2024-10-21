a46fe9ea56eba9d8f940900fef40104297e8fbd9
Min Wang <minwang807@gmail.com>
2024-10-21 12:56:26 +0200

    Se ha creado la pantalla de "Main" -Se puede pasar en la pantalla de login a una pantalla de main -Se puede pasar tambien desde la pantalla de signup a main dandole al boton de signup y al darle al boton de cancel, vuelve a login -En el main hay un actionbar que te lleva a login

--- app/src/main/java/com/min/nicestart/Singup.java
+++ app/src/main/java/com/min/nicestart/Singup.java
@@ -1,6 +1,8 @@
 package com.min.nicestart;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -20,5 +22,14 @@ public class Singup extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+    }
+    public void VolverLogin(View view) {
+        Intent intent = new Intent(Singup.this,Login.class);
+        startActivity(intent);
+    }
+
+    public void AbrirMain(View view) {
+        Intent intent = new Intent(Singup.this,Main.class);
+        startActivity(intent);
     }
 }
