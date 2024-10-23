759c2f06bb322464e045a0ee3a72d372faff4da0
Min Wang <minwang807@gmail.com>
2024-10-23 14:05:11 +0200

    Se ha añadido dos importaciones de librerias, se ha quetado el drawable del xml y se ha metido codigo en el .java para que saliera desde ahi

--- app/src/main/java/com/min/nicestart/Login.java
+++ app/src/main/java/com/min/nicestart/Login.java
@@ -1,19 +1,35 @@
 package com.min.nicestart;
 
 import android.content.Intent;
+import android.graphics.drawable.ColorDrawable;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.ImageView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
+import com.bumptech.glide.Glide;
+import com.bumptech.glide.load.resource.drawable.DrawableTransitionOptions;
 
 public class Login extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        ImageView mGirl = findViewById(R.id.girl);//girl del id del xml
+
+        Glide.with(this)
+             //   .load("https://images.unsplash.com/photo-1489424731084-a5d8b219a5bb")
+                .load(R.drawable.girl)//girl.png
+              //  .transition(DrawableTransitionOptions.withCrossFade(2000))
+               // .centerCrop()
+                //  .circleCrop()
+               // .placeholder(new ColorDrawable(this.getResources().getColor(R.color.amarillo)))
+                        .into(mGirl);
+
+
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.login);
