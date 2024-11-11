29e0b2c80a506e7e86814e9c820b9d0035dfcc81
Min Wang <minwang807@gmail.com>
2024-11-11 14:05:08 +0100

    Añadido unas opciones de menu en el Main view

--- app/src/main/java/com/min/nicestart/Main.java
+++ app/src/main/java/com/min/nicestart/Main.java
@@ -1,16 +1,27 @@
 package com.min.nicestart;
 
+import android.content.Context;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.ContextMenu;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.webkit.WebView;
 import android.widget.ImageView;
+import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.constraintlayout.widget.ConstraintLayout;
+import androidx.constraintlayout.widget.ConstraintSet;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
 import com.bumptech.glide.Glide;
-import com.bumptech.glide.load.resource.drawable.DrawableTransitionOptions;
+import com.google.android.material.snackbar.Snackbar;
 
 public class Main extends AppCompatActivity {
 
@@ -20,6 +31,10 @@ public class Main extends AppCompatActivity {
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
 
+        TextView mycontext=findViewById(R.id.texto);
+        registerForContextMenu(mycontext);
+
+
 
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
@@ -27,4 +42,60 @@ public class Main extends AppCompatActivity {
             return insets;
         });
     }
+    //implemento context menu
+    @Override
+    public void onCreateContextMenu(ContextMenu menu, View v,ContextMenu.ContextMenuInfo menuinfo)
+    {
+        getMenuInflater().inflate(R.menu.menu_context,menu);
+    }
+    @Override
+    public boolean onContextItemSelected(MenuItem item)
+    {
+        int id=item.getItemId();
+        if (id==R.id.item1) {
+            Toast.makeText(this, "ITEM COPIADO", Toast.LENGTH_SHORT).show();
+            return true;
+        }
+        else if( id==R.id.item2)
+        {
+            Toast.makeText(this, "DOWLOAD ITEM", Toast.LENGTH_SHORT).show();
+            return true;
+        }
+        else
+            return false;
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu)
+    {
+        getMenuInflater().inflate(R.menu.menu_appbar,menu);
+        return true;
+    }
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item)
+    {
+        int id=item.getItemId();//numero segun orden de creacion en xml
+        if(id==R.id.item1)
+        {
+            Intent intent = new Intent(Main.this, Profile.class);
+            startActivity(intent);
+        }
+        if(id==R.id.item2)
+        {
+            final ConstraintLayout layout=findViewById(R.id.main);
+            Snackbar snackbar = Snackbar
+                    .make(layout, "fancy a Snack while you refresh", Snackbar.LENGTH_LONG)
+                    .setAction("Action", new View.OnClickListener() {
+                        @Override
+                        public void onClick(View view) {
+                            Toast.makeText(Main.this, "Action is restaured", Toast.LENGTH_SHORT).show();
+                        }
+
+                    });
+    snackbar.show();
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
 }
