df52c781de3e57a7ce1c4f22606251bc53eacbb4
Min Wang <minwang807@gmail.com>
2025-01-22 13:18:26 +0100

    se ha creado el main_bn con 4 fragments, y el main_bab que se creo anteriormente

---
+++ app/src/main/java/com/min/nicestart/MainBab.java
@@ -0,0 +1,75 @@
+package com.min.nicestart;
+
+import android.os.Bundle;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.Toast;
+import android.widget.Toolbar;
+import com.min.nicestart.R;
+
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+import com.google.android.material.bottomappbar.BottomAppBar;
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+
+public class MainBab extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_mainbab);
+
+//        this.getSupportActionBar().setDisplayShowCustomEnabled(true);
+//
+//        LayoutInflater inflater = LayoutInflater.from(this);
+//        View v = inflater.inflate(R.layout.custom_action_bar, null);
+//
+//        TextView titleTextView = (TextView) v.findViewById(R.id.custom_action_bar_title);
+//        titleTextView.setText(this.getTitle());
+//        titleTextView.setTypeface(App.getInstance().getActionBarTypeFace());
+//
+//        this.getSupportActionBar().setCustomView(v);
+
+        // cast al xml
+        BottomAppBar bottomAppBar = findViewById(R.id.bottom_app_bar);
+        FloatingActionButton myfab = findViewById(R.id.fab);
+
+        //click event en el  FAB
+        myfab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Toast.makeText(MainBab.this, "FAB Clicked", Toast.LENGTH_SHORT).show();
+            }
+        });
+
+        //click event en el Hamburguer menu
+        bottomAppBar.setNavigationOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Toast.makeText(MainBab.this, "Menu clicked", Toast.LENGTH_SHORT).show();
+//                sheetBehavior = BottomSheetBehavior.from(sheet);
+            }
+
+
+        });
+
+        //click event en el Bottom bar menu item
+        bottomAppBar.replaceMenu(R.menu.bottom_app_bar_menu);
+
+        bottomAppBar.setOnMenuItemClickListener(item -> {
+            int id = item.getItemId(); // Obtén el ID del elemento del menú
+            if (id == R.id.corazon) {
+                Toast.makeText(MainBab.this, "Added to favourites", Toast.LENGTH_SHORT).show();
+            } else if (id == R.id.lenguaje) {
+                Toast.makeText(MainBab.this, "Beginning search", Toast.LENGTH_SHORT).show();
+            }
+            return true; // Devuelve true si manejaste el evento
+        });
+
+    }
+}
---
+++ app/src/main/java/com/min/nicestart/MainBn.java
@@ -0,0 +1,144 @@
+package com.min.nicestart;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.Toast;
+
+import androidx.annotation.IdRes;
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.viewpager.widget.ViewPager;
+
+import com.min.nicestart.ui.main.SectionsPagerAdapter;
+//import com.min.nicestart.databinding.ActivityMainBnBinding;
+import com.google.android.material.bottomnavigation.BottomNavigationItemView;
+import com.google.android.material.bottomnavigation.BottomNavigationMenuView;
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+
+
+public class MainBn extends AppCompatActivity {
+
+    // private ActivityMainBnBinding binding;
+    private MenuItem prevMenuItem;
+    private SectionsPagerAdapter sectionsPagerAdapter;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main_bn);
+//        binding = ActivityMainBnBinding.inflate(getLayoutInflater());
+//        setContentView(binding.getRoot());
+
+        //el adaptador coloca las Pages -los fragmentos con las diferentes vistas- dentro de la vista padre Viewpager del xml
+        sectionsPagerAdapter = new SectionsPagerAdapter(this, getSupportFragmentManager());
+//        ViewPager viewPager = binding.viewPager;
+        ViewPager viewPager1 = findViewById(R.id.view_pager);
+        viewPager1.setAdapter(sectionsPagerAdapter);
+//        TabLayout tabs = binding.tabs;
+//        tabs.setupWithViewPager(viewPager);
+//        FloatingActionButton fab = binding.fab;
+//
+//        fab.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View view) {
+//                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+//                        .setAction("Action", null).show();
+//            }
+    }
+}
+
+
+// cast al xml
+//        BottomNavigationView mybottomNavView = findViewById(R.id.bottom_navigation);
+
+//        // crear badges
+//        BottomNavigationMenuView bottomNavigationMenuView =
+//                (BottomNavigationMenuView) mybottomNavView.getChildAt(0);
+//        View v = bottomNavigationMenuView.getChildAt(2);
+//        BottomNavigationItemView itemView = (BottomNavigationItemView) v;
+//
+//        LayoutInflater.from(this)
+//                .inflate(R.layout.layout_badge, itemView, true);
+
+
+//        mybottomNavView.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
+//            @Override
+//            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+//
+//                switch (item.getItemId()) {
+//                    case R.id.corazon:
+//                        item.setChecked(true);
+//                        Toast.makeText(MainBn.this, "Likes clicked.", Toast.LENGTH_SHORT).show();
+//                        removeBadge(mybottomNavView,item.getItemId());
+//                        viewPager1.setCurrentItem(0);
+//                        break;
+//
+//                    case R.id.add:
+//                        item.setChecked(true);
+//                        Toast.makeText(MainBn.this, "Add clicked.", Toast.LENGTH_SHORT).show();
+//                        removeBadge(mybottomNavView,item.getItemId());
+//                        viewPager1.setCurrentItem(1);
+//                        break;
+//
+//                    case R.id.browse:
+//                        item.setChecked(true);
+//                        Toast.makeText(MainBn.this, "Browse clicked.", Toast.LENGTH_SHORT).show();
+//                        removeBadge(mybottomNavView,item.getItemId());
+//                        viewPager1.setCurrentItem(2);
+//                        break;
+//
+//                    case R.id.persona:
+//                        item.setChecked(true);
+//                        Toast.makeText(MainBn.this, "Personal clicked.", Toast.LENGTH_SHORT).show();
+//                        removeBadge(mybottomNavView,item.getItemId());
+//                        viewPager1.setCurrentItem(3);
+//                        break;
+//
+//                }
+//                return false;
+//            }
+//        });
+
+
+//        here we listen to viewpager moves and set navigations items checked or not
+
+//        viewPager1.addOnPageChangeListener(new ViewPager.OnPageChangeListener() {
+//            @Override
+//            public void onPageScrolled(int position, float positionOffset, int positionOffsetPixels) {
+//
+//            }
+
+//            @Override
+//            public void onPageSelected(int position) {
+//                if (prevMenuItem != null) {
+//                    prevMenuItem.setChecked(false);
+//                } else {
+//                    mybottomNavView.getMenu().getItem(0).setChecked(false);
+//                    mybottomNavView.getMenu().getItem(position).setChecked(true);
+//                    removeBadge(mybottomNavView, mybottomNavView.getMenu().getItem(position).getItemId());
+//                }
+//            }
+//
+//            @Override
+//            public void onPageScrollStateChanged(int state) {
+//
+//            }
+//        });
+//
+//    }
+
+//    /**
+//     * Remove badge.
+//     *
+//     * @param bottomNavigationView the bottom navigation view
+//     * @param itemId               the item id
+//     */
+//removing badges
+//    public static void removeBadge(BottomNavigationView bottomNavigationView, @IdRes int itemId) {
+//        BottomNavigationItemView itemView = bottomNavigationView.findViewById(itemId);
+//        if (itemView.getChildCount() == 3) {
+//            itemView.removeViewAt(2);
+//        }
+//    }
